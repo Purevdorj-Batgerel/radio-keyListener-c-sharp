732976e4791bf351923bdba80249c2f687674eed
Purevdorj <purvee.hq@gmail.com>
2017-12-14 17:49:06 +0800

    initial Commit

---
+++ keyListener/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace keyListener
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.trayMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.AboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.QuitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trayMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.BalloonTipText = "Radio-station balloon text";
+            this.trayIcon.BalloonTipTitle = "Radio-station balloon title";
+            this.trayIcon.ContextMenuStrip = this.trayMenuStrip;
+            this.trayIcon.Icon = global::keyListener.Properties.Resources.favicon;
+            this.trayIcon.Text = "Radio-station-TEXT";
+            this.trayIcon.Visible = true;
+            this.trayIcon.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // trayMenuStrip
+            // 
+            this.trayMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AboutToolStripMenuItem,
+            this.QuitToolStripMenuItem});
+            this.trayMenuStrip.Name = "trayMenuStrip";
+            this.trayMenuStrip.Size = new System.Drawing.Size(108, 48);
+            // 
+            // AboutToolStripMenuItem
+            // 
+            this.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem";
+            this.AboutToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.AboutToolStripMenuItem.Text = "About";
+            this.AboutToolStripMenuItem.Click += new System.EventHandler(this.AboutToolStripMenuItem_Click);
+            // 
+            // QuitToolStripMenuItem
+            // 
+            this.QuitToolStripMenuItem.Name = "QuitToolStripMenuItem";
+            this.QuitToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.QuitToolStripMenuItem.Text = "Quit";
+            this.QuitToolStripMenuItem.Click += new System.EventHandler(this.QuitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 250);
+            this.Icon = global::keyListener.Properties.Resources.favicon;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "About";
+            this.trayMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip trayMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem AboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem QuitToolStripMenuItem;
+    }
+}
+
---
+++ keyListener/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace keyListener {
+    public partial class Form1 : Form {
+        private bool allowVisible = false;
+        private bool allowClose = false;
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        protected override void SetVisibleCore(bool value) {
+            if (!allowVisible) {
+                value = false;
+                if (!IsHandleCreated) CreateHandle();
+            }
+            base.SetVisibleCore(value);
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e) {
+            if (!allowClose) {
+                Hide();
+                e.Cancel = true;
+            }
+            base.OnFormClosing(e);
+        }
+
+        public void Act(int vkCode) {
+            bool mediaPressed = true;
+            string route = "";
+            switch((Keys)vkCode) {
+                case Keys.MediaPlayPause:
+                    route = "PlayPause";
+                    break;
+                case Keys.MediaNextTrack:
+                    route = "NextTrack";
+                    break;
+                case Keys.MediaPreviousTrack:
+                    route = "PreviousTrack";
+                    break;
+                default:
+                    mediaPressed = false;
+                    break;
+            }
+            if(mediaPressed) {
+                var request = (HttpWebRequest)WebRequest.Create("localhost:8000/" + route);
+
+                var postData = "request=true";
+
+                var data = Encoding.ASCII.GetBytes(postData);
+
+                request.Method = "POST";
+                request.ContentType = "application/x-www-form-urlencoded";
+                request.ContentLength = data.Length;
+
+                using (var stream = request.GetRequestStream()) {
+                    stream.Write(data, 0, data.Length);
+                }
+
+                var response = (HttpWebResponse)request.GetResponse();
+                var respocseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+            }
+        }
+
+        private void AboutToolStripMenuItem_Click(object sender, EventArgs e) {
+            allowVisible = true;
+            Show();
+        }
+
+        private void QuitToolStripMenuItem_Click(object sender, EventArgs e) {
+            allowClose = true;
+            Application.Exit();
+        }
+    }
+}
